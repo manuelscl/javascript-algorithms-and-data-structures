c2e3937035a3586bf75127931bc946d4b8ea72d7
manuelscl <manuelscardenaslopez@gmail.com>
2023-10-10 23:12:06 -0600

    Module 1 / Variables in Javascript
    In this module I have learned:
    -Comments
    -Javascript variables
    -Storing values with the assignment operator
    -Assigning the value of one variable to another
    -Initializing variables with the assignment operator
    -Declare string variables
    -Understanding uninitialized variables
    -Understanding case sensitivity in variables
    -Explore differences between the var y let keywords
    -Declare a read-only variable with the const keyword
    -Add two numbers with javascript

---
+++ modulo-1/script.js
@@ -0,0 +1,65 @@
+// This is an in-line comment.
+
+/* This is a 
+multi-line comment */
+
+
+// JavaScript Variables
+var myName;
+
+
+// Storing Values with the Assignment Operator
+var a;
+a = 7;
+
+// Assigning the Value of One Variable to Another
+var b;
+b = a;
+
+// Initializing Variables with the Assignment Operator
+var a = 9;
+
+// Declare String Variables
+var myFirstName = "Manuel";
+var myLastName = "Cárdenas";
+
+// Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Understanding Case Sensitivity in Variables
+
+// Variable declarations
+
+var StUdLyCapVaR; // => studlyCapVar
+var properCamelCase; // => properCamelCase
+var TitleCaseOver; // => titleCaseOver
+
+// Variable assignments
+
+STUDLYCAPVAR = 10; // => studlyCapVar
+PRoperCAmelCAse = "A String"; // => properCamelCase
+tITLEcASEoVER = 9000; // => titleCaseOver
+
+
+// Explore Differences Between the var and let Keywords
+var myName = "Oliver";
+myName = "David";
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// Declare a Read-Only Variable with the const Keyword
+
+const FCC = "freeCodeCamp"; // You should use const and change fCC to all uppercase.
+let fact = "is cool!"; // Use let instead of var
+fact = "is awesome!";
+console.log(FCC, fact);
+
+// Add Two Numbers with JavaScript
+const sum = 10 + 10;
