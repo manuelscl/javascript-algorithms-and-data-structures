1b0fe2029a44ba5d163458bbba4a7a3a8a8b195f
manuelscl <manuelscardenaslopez@gmail.com>
2023-10-10 23:23:46 -0600

    Module 2 / Javascript algorithms
    -Subtract one number from another with javascript
    -Multiply two numbers
    -Divide one number by another
    -Increment a number
    -Decrement a number
    -Create decimal numbers
    -Multiply two decimals
    -Divide one decimal by another with javascript
    -Finding a remainder in javascript
    -Compound assignment with augmented addition
    -Compound assignment with augmented subtraction
    -Compound assignment with augmented multiplication
    -Compound assignment with augmented division

---
+++ module-2/script.js
@@ -0,0 +1,72 @@
+// Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+
+// Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+// Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+// Increment a Number with JavaScript
+let myVar = 87;
+
+myVar = myVar + 1;
+myVar ++;
+
+
+// Decrement a Number with JavaScript
+myVar = 11;
+
+myVar = myVar - 1;
+myVar --;
+
+
+// Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+const myDecimal = 5.7;
+
+// Multiply Two Decimals with JavaScript
+const productWithDecimals = 2.0 * 2.5;
+
+// Divide One Decimal by Another with JavaScript
+const quotient2 = 4.4 / 2.0;
+
+// Finding a Remainder in JavaScript
+const remainder = 11 % 3;
+
+// Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+// Compound Assignment With Augmented Subtraction
+let num = 11;
+let num2 = 9;
+let num3 = 3;
+
+num -= 6;
+num2 -= 15;
+num3 -= 1;
+
+
+// Compound Assignment With Augmented Multiplication
+num = 5;
+num2 = 12;
+num3 = 4.6;
+
+num *= 5;
+num2 *= 3;
+num3 *= 10;
+
+// Compound Assignment With Augmented Division
+num = 48;
+num2 = 108;
+num3 = 33;
+
+num /= 12;
+num2 /= 4;
+num3 /= 11;
