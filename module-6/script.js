59226b5874ffbc334592e2caea2bad591f5e7c95
manuelscl <manuelscardenaslopez@gmail.com>
2023-10-17 22:21:20 -0600

    Module 6 / Using Switch
    -Selecting from many options with switch statements
    -Adding a default option in switch  statements
    -Multiple identical options in switch statements
    -Replacing if else chains with switch

---
+++ module-6/script.js
@@ -0,0 +1,97 @@
+// Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+  let answer = "";
+
+  switch(val) {
+    case 1:
+      answer = "alpha";
+    break;
+    case 2:
+      answer = "beta";
+    break;
+    case 3:
+      answer = "gamma";
+    break;
+    case 4:
+      answer = "delta";
+    break;
+  }
+
+  return answer;
+}
+
+caseInSwitch(1);
+
+
+// Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  let answer = "";
+  switch(val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  return answer;
+}
+
+switchOfStuff(1);
+
+// Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  let answer = "";
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+
+sequentialSizes(1);
+
+// Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
+
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+
+chainToSwitch(7);
